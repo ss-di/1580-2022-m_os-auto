68b5f3b98d8fe0dd4f3de16b8fe993cd697499d1
Alexander Lobachev <ss_di@mail.ru>
2022-11-30 23:04:58 +0300

    пердначало...

---
+++ bin/install.sh
@@ -0,0 +1,8 @@
+!#/bin/sh
+
+echo '#!/bin/sh' > /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
+echo "cd /root/1580-2022-m_os-auto" >>/etc/rc.d/rc.local
+echo "git pull" >>/etc/rc.d/rc.local
+
+/bin/sh bin/main.sh
---
+++ bin/main.sh
@@ -0,0 +1,4 @@
+!#/bin/sh
+
+#rm -rf /home/student
+#tar xjvpf data/home-student.tbz -C /home/
