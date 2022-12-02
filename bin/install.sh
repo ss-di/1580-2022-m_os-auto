cac8ea17e359cb69aa2585a2e1a19ad99ffb8854
Alexander Lobachev <ss_di@mail.ru>
2022-12-02 09:48:59 +0300

    add main in rc.local

--- bin/install.sh
+++ bin/install.sh
@@ -4,5 +4,5 @@ echo '#!/bin/sh' > /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 echo "cd /root/1580-2022-m_os-auto" >>/etc/rc.d/rc.local
 echo "git pull" >>/etc/rc.d/rc.local
+echo "/bin/sh /root/1580-2022-m_os-auto/bin/main.sh" >>/etc/rc.d/rc.local
 
-#/bin/sh bin/main.sh
