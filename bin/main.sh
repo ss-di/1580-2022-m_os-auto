20baee8be7ff502612bc2b26213d52e9216e223d
Alexander Lobachev <ss_di@mail.ru>
2022-12-02 14:26:58 +0300

    alfa2

--- bin/main.sh
+++ bin/main.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-cp data/wallpapers/1580-warning.jpg /usr/local/share/1580.jpg
+cp data/wallpapers/1580-warning.jpg /usr/share/design/school/backgrounds/default.png
 
 rm -rf /home/student
 tar xjvpf data/homes/home-student-empty.tbz -C /home/
-
-echo "[Wallpapers]" > /home/student/.config/plasmarc
-echo "usersWallpapers=/usr/local/share/1580.jpg" >> /home/student/.config/plasmarc
 
 at now +1 minutes -f /root/1580-2022-m_os-auto/bin/update.sh
