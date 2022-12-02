60ccfd76684cc11c26349790dd58c69281208266
Alexander Lobachev <ss_di@mail.ru>
2022-12-02 17:09:56 +0300

    Попробуем автообновление.
    Надеюсь рухнет на все сразу

--- bin/update.sh
+++ bin/update.sh
@@ -3,6 +3,7 @@
 cd /root/1580-2022-m_os-auto/
 git pull
 
+apt-get update && apt-get -y dist-upgrade && update-kernel -f && apt-get clean
 apt-get -y install pascalabcnet
 apt-get -y install scratch-desktop
 epm ei
