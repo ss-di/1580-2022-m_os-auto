4538b354e69043d52efca72e7a914cce16031bc0
Alexander Lobachev <ss_di@mail.ru>
2022-12-02 13:12:54 +0300

    one more test

--- bin/install.sh
+++ bin/install.sh
@@ -4,5 +4,5 @@ echo '#!/bin/sh' > /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 
 echo "cd /root/1580-2022-m_os-auto" >> /etc/rc.d/rc.local
-echo "sh /root/1580-2022-m_os-auto/bin/main.sh" >> /etc/rc.d/rc.local
+echo "sh bin/main.sh" >> /etc/rc.d/rc.local
 
--- bin/main.sh
+++ bin/main.sh
@@ -4,5 +4,6 @@
 #tar xjvpf data/home-student.tbz -C /home/
 
 touch /tmp/111-test-222
+touch /tmp/123-test-321
 
 at now +1 minutes -f /root/1580-2022-m_os-auto/bin/update.sh
