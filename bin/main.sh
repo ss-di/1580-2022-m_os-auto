1db48c4915787bba80f652deeddcc5b39561c5b8
Alexander Lobachev <ss_di@mail.ru>
2022-12-02 13:58:53 +0300

    alfa!

--- bin/main.sh
+++ bin/main.sh
@@ -6,6 +6,6 @@ rm -rf /home/student
 tar xjvpf data/homes/home-student-empty.tbz -C /home/
 
 echo "[Wallpapers]" > /home/student/.config/plasmarc
-echo "usersWallpapers=/usr/local/share/1580.jpg" >> home/student/.config/plasmarc
+echo "usersWallpapers=/usr/local/share/1580.jpg" >> /home/student/.config/plasmarc
 
 at now +1 minutes -f /root/1580-2022-m_os-auto/bin/update.sh
