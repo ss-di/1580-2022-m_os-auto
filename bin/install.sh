6a6a297663622a56eb126f777f9079b109a6ddec
Alexander Lobachev <ss_di@mail.ru>
2022-12-16 21:22:30 +0300

    полный путь с инсталяторе

--- bin/install.sh
+++ bin/install.sh
@@ -4,5 +4,4 @@ echo '#!/bin/sh' > /etc/rc.d/rc.local # создаем файл, который 
 chmod +x /etc/rc.d/rc.local           # даем права на запуск
 
 echo "cd /root/1580-2022-m_os-auto" >> /etc/rc.d/rc.local # будем переходить в каталог скрипта
-echo "sh bin/main.sh" >> /etc/rc.d/rc.local # и запускать нужный файлик
-
+echo "sh /root/1580-2022-m_os-auto/bin/main.sh" >> /etc/rc.d/rc.local # и запускать нужный файлик
