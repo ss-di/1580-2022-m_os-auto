38327aa7c2ddd53245f0f44dfb1fba9ec127b13a
Alexander Lobachev <ss_di@mail.ru>
2023-01-16 19:39:17 +0300

    немного подкрутили фильтр инета в предверии региона

--- bin/update2.sh
+++ bin/update2.sh
@@ -10,10 +10,12 @@ do
 done
 
 inet_filter(){
-    for i in `cat data/white_site.lst`
-    do
-        iptables -A OUTPUT -m string --string $i --algo kmp -j ACCEPT
-    done
+    # iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT # ответы на исходящие пакеты
+
+    iptables -A OUTPUT -p udp --dport 53 -j ACCEPT # DNS
+   
+    iptables -A OUTPUT -d 194.58.88.173 -j ACCEPT # olympiads.ru - для региона
+    iptables -A OUTPUT -d 84.201.160.168 -j ACCEPT # ejudge.msk.ru - для региона
 
     iptables -A OUTPUT -d 81.177.135.190 -j ACCEPT # разрешаем sdo.1580.ru
 
@@ -28,6 +30,12 @@ inet_filter(){
 
     iptables -A OUTPUT -d 88.212.202.50 -j ACCEPT # разрешаем www.liveinternet.ru
 
+
+    for i in `cat data/white_site.lst`
+    do
+        iptables -A OUTPUT -m string --string $i --algo kmp -j ACCEPT
+    done
+
     for i in `cat data/black_site.lst`
     do
         iptables -A OUTPUT -m string --string $i --algo kmp -j REJECT
@@ -35,11 +43,9 @@ inet_filter(){
 }
 
 inet_white_only(){
-    # iptables -A OUTPUT -p tcp --dport 80 -j LOG # блокируем исходящий http
-    # iptables -A OUTPUT -p tcp --dport 443 -j LOG # блокируем исходящий https
-
     iptables -A OUTPUT -p tcp --dport 80 -j DROP # блокируем исходящий http
     iptables -A OUTPUT -p tcp --dport 443 -j DROP # блокируем исходящий https
+    iptables -p OUTPUT DROP # блокируем всё исходящее, кроме разрешенного
 }
 
 if [ "`hostname | grep localhost`" ] # для не настроенных
@@ -62,7 +68,7 @@ then
     # ничего не делаем
     echo do nothing
 
-elif [ "`hostname | grep m1580-2-418`" ] # для моноблоков 2-418
+elif [ "`hostname | grep m1580-2`" ] # для моноблоков 2-го корпуса
 then
     inet_filter
 #    inet_white_only
