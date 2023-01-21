4b2058cb6412505410fab9eeaee248457e4b3485
Alexander Lobachev <ss_di@mail.ru>
2023-01-21 19:52:13 +0300

    обновил скрипт установки VirtualBox

--- bin/update_tasks/0000035-bl_vboxnetflt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# exit
-
-# exec >> /var/log/update-sh.log 2>&1 # перенаправляем весь вывод в лог для анализа чего не рабтает
-
-echo blacklist vboxnetflt > /etc/modprobe.d/blacklist-vboxnetflt.conf || exit 1
