e39bcef66ecc3b9535a3a14d2ed40c659a45cde3
Alexander Lobachev <ss_di@mail.ru>
2023-01-30 00:33:34 +0300

    fix trik

--- bin/update_tasks/0000057-trikStudio.sh
+++ bin/update_tasks/0000057-trikStudio.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+apt-get -y install lego-mindstorms-udev-rules || exit 1
 apt-get -y install trikStudio || exit 1
 
 gpasswd -a student mindstormusers
--- bin/update_tasks/0000058-trikStudioJunior.sh
+++ bin/update_tasks/0000058-trikStudioJunior.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+apt-get -y install lego-mindstorms-udev-rules || exit 1
 apt-get -y install trikStudioJunior || exit 1
