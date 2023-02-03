84f408d1ebe14f3d1b97e0785f4dae5a05002709
Alexander Lobachev <ss_di@mail.ru>
2023-02-03 16:34:54 +0300

    teacher default 418-3

--- bin/update_tasks/0000066-gia_on-418-3.sh
+++ bin/update_tasks/0000066-gia_on-418-3.sh
@@ -2,7 +2,7 @@
 
 [ ! "`hostname | grep m1580-2-418-3`" ] && exit
 
-[ ! "`cat /etc/X11/sddm/sddm.conf | grep teacher`" ] && exit
+[ "`cat /etc/X11/sddm/sddm.conf | grep teacher`" ] && exit
 
 sed -i'.bak' -E -e 's,^Session.+,Session=plasma,' -e 's,^User.+,User=teacher,' /etc/X11/sddm/sddm.conf
 reboot
