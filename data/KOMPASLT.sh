f3822a326ffec6574714dd2be2d2488a8b31c861
Alexander Lobachev <ss_di@mail.ru>
2023-03-20 11:12:11 +0300

    fix: kompas runner

--- data/KOMPASLT.sh
+++ data/KOMPASLT.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Распакуем архив, если нужно.
+[ ! -d /home/student/.wine/ ] && tar xjpf /home/student-wine-kompas_3d_lt.tbz -C /home/student/
+
 wine "/home/student/.wine/drive_c/Program Files/ASCON/KOMPAS-3D LT V12/Bin/KOMPASLT.Exe"
