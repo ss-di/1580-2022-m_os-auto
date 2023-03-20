bfa011ab7d2acf3ca1a4e63258c72fe479f73bb3
Alexander Lobachev <ss_di@mail.ru>
2023-03-20 11:18:21 +0300

    fix: dl

--- bin/update_tasks/0120001-dl_student-wine-PABC.sh
+++ bin/update_tasks/0120001-dl_student-wine-PABC.sh
@@ -2,7 +2,7 @@
 
 $FILE=student-wine-PABC.tbz
 
-cd /home/ && wget -nc https://sdo.1580.ru/some-files/$FILE && \
-             wget -nc https://sdo.1580.ru/some-files/$FILE.md5 && \
-             md5sum -c $FILE.md5 || (rm -f $FILE*; exit 1)
+cd /home/ && wget -nc "https://sdo.1580.ru/some-files/$FILE" && \
+             wget -nc "https://sdo.1580.ru/some-files/$FILE.md5" && \
+             md5sum -c "$FILE.md5" || (rm -f "$FILE*"; exit 1)
 
