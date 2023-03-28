b7f03fbb7a3a2ef39161c31ed7c2fd5fff054c2b
Alexander Lobachev <ss_di@mail.ru>
2023-03-28 15:32:38 +0300

    feat: добавим obs-studio

---
+++ bin/update_tasks/0000081-obs-studio.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# обновление системы от разработчиков
+apt-get update || exit 1
+apt-get -y dist-upgrade || exit 1
+update-kernel -f || exit 1
+apt-get clean || exit 1
+
+apt-get -y install obs-studio || exit 1
