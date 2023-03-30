f709193f79efd8e63e2eb82436efa13f7b5e0ae3
Alexander Lobachev <ss_di@mail.ru>
2023-03-30 13:35:11 +0300

    feat: it-host for console

--- bin/it-hostname-cli.sh
+++ bin/it-hostname-cli.sh
@@ -5,8 +5,8 @@ then
 
     myhostname=$(hostname)
 
-    mylab=$(kdialog --title="Настройка системы" --inputbox "Введите номер лаборатории (1, 2, 3, 4)")
-    mynum=$(kdialog --title="Настройка системы" --inputbox "Введите номер компьютера (1, 2, ...)")
+    mylab=$1
+    mynum=$2
     myhostname = "custom-2-it$mylab-$mynum"
 
     echo $myhostname
