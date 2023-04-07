6b1a86fc13cabe7de36638634d32cb90cd2aa06f
Alexander Lobachev <ss_di@mail.ru>
2023-04-07 17:09:11 +0300

    fix: W10VM

--- data/Windows10vm.sh
+++ data/Windows10vm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Распакуем винду, если нужно.
-#[ ! -d /home/teacher/VirtualBox\ VMs/Windows\ 10/ ] && VBoxManage import /home/Windows_10.ova
+#[ ! -d ~/VirtualBox\ VMs/Windows\ 10/ ] && VBoxManage import /home/Windows_10.ova
 
 VBoxManage startvm "Windows 10"
