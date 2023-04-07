2f53aa36ad14ea48b8db5201f4e673b630f946d8
Alexander Lobachev <ss_di@mail.ru>
2023-04-07 16:19:28 +0300

    fix hit

---
+++ bin/hit.sh
@@ -0,0 +1,18 @@
+!/bin/sh
+
+if [[ $(whoami) == 'root' ]]
+then
+
+    myhostname=$(hostname)
+
+    mylab=$1
+    mynum=$2
+    myhostname="n1580-2-it$mylab-$mynum"
+
+    echo $myhostname
+
+    pkexec bash -c 'rm -f /etc/machine-id && rm -f /var/lib/dbus/machine-id && dbus-uuidgen --ensure && systemd-machine-id-setup && hostnamectl hostname '$myhostname' && reboot'
+
+else
+    echo 'Требуется запускать от суперпользователя'
+fi
