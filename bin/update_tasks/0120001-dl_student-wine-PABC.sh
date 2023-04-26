334fb309848e1caf53e369214acba0b3c19e8dcb
Alexander Lobachev <ss_di@mail.ru>
2023-04-26 14:51:32 +0300

    fix pabc dounload

--- bin/update_tasks/0120001-dl_student-wine-PABC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-FILE=student-wine-PABC.tbz
-
-cd /home/ && wget -nc https://sdo.1580.ru/some-files/$FILE && \
-             wget -nc https://sdo.1580.ru/some-files/$FILE.md5 && \
-             md5sum -c $FILE.md5 || (rm -f $FILE*; exit 1)
-
